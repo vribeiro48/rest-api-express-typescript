c10aea715c12c730258107342eea36c200219422
Vinicius Ribeiro <vinicius.ribeiro@contraprova.com.br>
2023-10-12 20:05:16 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
